63f7a5405147851717c971fc73b420c6086cc949
Yauhen Kib <zhen.kib@gmail.com>
2018-09-27 04:10:05 +0300

    change redirect after registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/tutor/dashboard';
+    protected $redirectTo = '/tutor/profile';
 
     /**
      * Create a new controller instance.
